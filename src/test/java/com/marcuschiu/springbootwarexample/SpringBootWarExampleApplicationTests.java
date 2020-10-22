d08f8dd82719b8d3d1530f4329158dcb88e4b595
Marcus Chiu <marcuschiu9@gmail.com>
2020-10-22 16:52:49 -0500

    initial commit

---
+++ src/test/java/com/marcuschiu/springbootwarexample/SpringBootWarExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.springbootwarexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWarExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
